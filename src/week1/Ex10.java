65e30f209f57cd2b72c3c420e156438d04df13c1
Daniil <danikvod@gmail.com>
2022-09-05 17:44:59 +0300

    Ex10

---
+++ src/week1/Ex10.java
@@ -0,0 +1,15 @@
+package week1;
+
+import java.util.*;
+
+public class Ex10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the radius - ");
+
+        //final double pi = 3.14;
+        int r = sc.nextInt();
+
+        System.out.println(2 * Math.PI * r);
+    }
+}
