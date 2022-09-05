034ad18248ccfe76bccac58ed99e08b1b07c2dd0
Daniil <danikvod@gmail.com>
2022-09-06 00:22:26 +0300

    Ex14

---
+++ src/week1/Ex14.java
@@ -0,0 +1,18 @@
+package week1;
+
+import java.util.*;
+
+public class Ex14 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Type a number - ");
+
+        int a = sc.nextInt();
+        if (a >= 0) {
+            System.out.println("The number is positive");
+        }else {
+            System.out.println("The number is saad");
+        }
+    }
+}
