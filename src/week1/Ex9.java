305013a912a4ff6c301584311636041c6d09ed91
Daniil <danikvod@gmail.com>
2022-09-05 17:35:17 +0300

    Ex9

---
+++ src/week1/Ex9.java
@@ -0,0 +1,14 @@
+package week1;
+
+import java.util.*;
+
+public class Ex9 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first num - ");
+        float a = sc.nextInt();
+        System.out.println("Enter second num - ");
+        float b = sc.nextInt();
+        System.out.println("Division = " + a/b);
+    }
+}
