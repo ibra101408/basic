5435bd6f8de317a18e8cde0539edfcc4d869e737
Daniil <danikvod@gmail.com>
2022-09-09 12:07:30 +0300

    Ex30

---
+++ src/week2/Ex30.java
@@ -0,0 +1,16 @@
+package week2;
+
+import java.util.Scanner;
+
+public class Ex30 {
+    public static void main(String[] args) {
+        Scanner cs = new Scanner(System.in);
+
+        int t = 4;
+        int i = 0;
+        while (i <= t){
+            i++;
+            System.out.println(i);
+        }
+    }
+}
