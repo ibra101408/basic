7ce661c7cd229019971e05f415c2615a57b9e9d4
Daniil <danikvod@gmail.com>
2022-09-09 11:35:58 +0300

    Ex79

---
+++ src/week4/NumberStatistics.java
@@ -0,0 +1,21 @@
+package week4;
+
+public class NumberStatistics {
+
+    private int amountOfNumbers;
+
+    public NumberStatistics() {
+        // initialize here the object variable amountOfNumbers
+        this.amountOfNumbers = 0;
+
+    }
+
+    public void addNumber(int number) {
+        amountOfNumbers
+    }
+
+    public int amountOfNumbers() {
+        return this.amountOfNumbers;
+    }
+}
+
