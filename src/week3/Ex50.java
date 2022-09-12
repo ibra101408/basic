d082f0619022adb89ea4c2bb68d4879dc4a74edc
Daniil <danikvod@gmail.com>
2022-09-12 22:02:38 +0300

    Ex50

---
+++ src/week3/Ex50.java
@@ -0,0 +1,16 @@
+package week3;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Ex50 {
+
+    public static void main(String[] args) {
+        char ch;
+        String str = "text";
+        for(int i=0; i<str.length(); i++){
+            ch=str.charAt(i);
+            System.out.println(ch);
+        }
+    }
+}
