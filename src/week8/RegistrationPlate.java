56fb5e3b8b3e6d70a0eaa5d31cb1323d43070e86
Daniil <danikvod@gmail.com>
2022-09-16 15:29:57 +0300

    Ex9

---
+++ src/week8/RegistrationPlate.java
@@ -0,0 +1,49 @@
+package week8;
+
+public class RegistrationPlate {
+
+
+    private final String regCode;
+    private final String country;
+
+    public RegistrationPlate(String regCode, String country) {
+        this.regCode = regCode;
+        this.country = country;
+    }
+
+    public String toString() {
+        return country + " " + regCode;
+    }
+
+    @Override
+    public int hashCode() {
+        if(this.regCode == null){
+            return 7;
+        }
+        return this.country.hashCode() + this.regCode.hashCode();
+    }
+
+
+    @Override
+    public boolean equals(Object object) {
+        if (object == null) {
+            return false;
+        }
+
+        if (getClass() != object.getClass()) {
+            return false;
+        }
+
+        RegistrationPlate compared = (RegistrationPlate) object;
+
+        if (this.country.equals(compared.country)) {
+            return false;
+        }
+
+        if (this.regCode == null || this.regCode != compared.regCode) {
+            return false;
+        }
+
+        return true;
+    }
+}
