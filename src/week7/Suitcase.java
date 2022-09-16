0d7189cd4df1e0aa8328622a30128e4327cee3b1
Daniil <danikvod@gmail.com>
2022-09-16 11:12:38 +0300

    Ex4

---
+++ src/week7/Suitcase.java
@@ -0,0 +1,38 @@
+package week7;
+
+import java.util.ArrayList;
+
+public class Suitcase {
+
+    private ArrayList<Thing> things;
+    private int weightLimit;
+
+    public Suitcase(int weightLimit) {
+        this.weightLimit = weightLimit;
+        this.things = new ArrayList<Thing>();
+    }
+
+    public void addThing(Thing thing){
+        int weightCurrent = 0;
+        for(Thing currentThing : this.things){
+            weightCurrent += currentThing.getWeight();
+            }
+        if(weightCurrent + thing.getWeight() <= this.weightLimit){
+            this.things.add(thing);
+        }
+
+    }
+
+
+    @Override
+    public String toString() {
+        int weightCurrent = 0;
+        for(Thing currentThing : this.things){
+            weightCurrent += currentThing.getWeight();
+        }
+        if (weightCurrent == 0){
+            return "empty(" + weightCurrent + " kg)";
+        }
+        return this.things.size() + " things (" + weightCurrent + ")";
+    }
+}
